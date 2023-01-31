7f48b1ba3860a535e5796b4926c4d7cc19ecf47e
Sabi Viji <sabipalathinkal10@gmail.com>
2023-01-31 19:54:09 +0530

    firebase added

---
+++ firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCWxDHxvaHGtIrRiLDxeo3WLx7HdYuE1c8",
+  authDomain: "hope-b99cf.firebaseapp.com",
+  projectId: "hope-b99cf",
+  storageBucket: "hope-b99cf.appspot.com",
+  messagingSenderId: "789887367839",
+  appId: "1:789887367839:web:293b11bce8a9c011d4c0aa",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
