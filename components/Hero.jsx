1a4e280cd9168c9f3be8c2ffe7efd25956391bab
jesi318 <jeswin.p.jaison@gmail.com>
2023-02-05 13:15:18 +0530

    changed ui of home page

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,17 +1,43 @@
 import Link from "next/link";
 import React from "react";
-
+import Image from "next/image";
+import mypic from "../assets/in.png";
 const Hero = ({ heading, message }) => {
   return (
-    <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img">
+    <div className="h-screen mb-12 bg-fixed bg-center bg-cover custom-img pt-28">
       {/* overlay */}
-      <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]"></div>
+      <div className=" flex flex-column justify-center py-5">
+        <Image
+          src={mypic}
+          alt="Picture of the author"
+          width={100}
+          height={100}
+        />
+      </div>
+      <div className=" flex-column items-center py-2">
+        <div className="flex justify-center">
+          <h2 className="pl-6 text-5xl font-bold text-pink-600">4</h2>
+          <h2 className=" text-5xl font-bold text-blue-900">hope</h2>
+          <Image
+            src={mypic}
+            alt="Picture of the author"
+            width={50}
+            height={50}
+          />
+        </div>
 
-      <div className="p-5 text-white z-[2] ">
-        <h2 className="text-5xl font-bold">{heading}</h2>
-        <p className="py-5 text-xl">{message}</p>
-        <button className="px-8 py-2 border">
-          <Link href={"/about"}>About Us</Link>
+        <p className="px-5 py-5 text-xl">
+          Build the next level of career with world-class resume
+        </p>
+      </div>
+      <div className="mt-5 px-28 py-8 flex-column items-center">
+        <button className="px-8 py-2 border w-45">
+          <Link href={"/about"}>Login</Link>
+        </button>
+      </div>
+      <div className="mt-5 px-24 flex-column items-center">
+        <button className="px-8 py-2 border w-36">
+          <Link href={"/about"}>Sign Up</Link>
         </button>
       </div>
     </div>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -29,11 +29,12 @@ const Navbar = () => {
       style={{ backgroundColor: `${color}` }}
       className="fixed left-0 top-0 w-full z-10 ease-in duration-300"
     >
-      <div className="max-w-[1240px] m-auto flex justify-between items-center p-4 text-white">
+      <div className="max-w-[1240px] m-auto flex justify-between items-center p-4 text-blue-900">
         <Link href="/">
-          <h1 style={{ color: `${textColor}` }} className="font-bold text-4xl">
-            4HOPE
-          </h1>
+          <h1
+            style={{ color: `${textColor}` }}
+            className="font-bold text-4xl"
+          ></h1>
         </Link>
         <ul style={{ color: `${textColor}` }} className="hidden sm:flex">
           <li className="p-4">
